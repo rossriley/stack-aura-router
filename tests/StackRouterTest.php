3777f8b3b1c1c05b8679fb0ab841cc58209af84e
Ross Riley <riley.ross@gmail.com>
2014-03-26 11:36:11 +0000

    test for route resources

--- tests/StackRouterTest.php
+++ tests/StackRouterTest.php
@@ -10,7 +10,10 @@ class StackRouterTest extends \PHPUnit_Framework_TestCase
 
     public function testSimpleRoute() {
         $testapp = new Fixtures\TestApp;
-        $testapp = new RequestRouter($testapp, $this->getRouter());
+        $router = $this->getRouter();
+        $router->add('default', '/{controller}/{action}/{id}');
+        $testapp = new RequestRouter($testapp, $router);
+
         $response = $testapp->handle(Request::create("/test/test/123","GET"));
         $params = unserialize($response->getContent());
         $this->assertEquals("test", $params["route"]["controller"]);
@@ -20,7 +23,13 @@ class StackRouterTest extends \PHPUnit_Framework_TestCase
 
     public function testComplexRoute() {
         $testapp = new Fixtures\TestApp;
-        $testapp = new RequestRouter($testapp, $this->getRouter());
+        $router = $this->getRouter();
+        $router->add('complex', '/complex/{controller}/{action}/{id}{format}')
+               ->addTokens([
+                    'id'     => '[0-9a-z\-]+',
+                    'format' => '(\.[^/]+)?',
+        ]);
+        $testapp = new RequestRouter($testapp, $router);
         $response = $testapp->handle(Request::create("/complex/test/test/abc-123.json","GET"));
         $params = unserialize($response->getContent());
         $this->assertEquals("test",     $params["route"]["controller"]);
@@ -29,15 +38,20 @@ class StackRouterTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(".json",    $params["route"]["format"]);
     }
 
+    public function testResourceRoute() {
+        $router = $this->getRouter();
+        $router->attachResource('resources', '/resources');
+        $testapp = new Fixtures\TestApp;
+        $testapp = new RequestRouter($testapp, $router);
+        $response = $testapp->handle(Request::create("/resources/300/edit","GET"));
+        $params = unserialize($response->getContent());
+        $this->assertEquals("resources",     $params["route"]["controller"]);
+        $this->assertEquals("GET",           $params["route"]["REQUEST_METHOD"]);
+    }
+
     protected function getRouter() {
         $router_factory = new RouterFactory;
         $router = $router_factory->newInstance();
-        $router->add('default', '/{controller}/{action}/{id}');
-        $router->add('complex', '/complex/{controller}/{action}/{id}{format}')
-            ->addTokens(array(
-                'id'     => '[0-9a-z\-]+',
-                'format' => '(\.[^/]+)?',
-        ));
         return $router;
     }
 
